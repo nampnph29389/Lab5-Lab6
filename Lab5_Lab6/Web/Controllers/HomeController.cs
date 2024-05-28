0c9b8ea4273073551d170c80b27634288f538f6e
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2024-05-28 14:36:52 +0700

    Add files via upload

--- Lab5_Lab6/Web/Controllers/HomeController.cs
+++ Lab5_Lab6/Web/Controllers/HomeController.cs
@@ -1,32 +1,44 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Web.Models;
 
-namespace Web.Controllers;
-
-public class HomeController : Controller
+namespace Web.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var urlRequest = "https://localhost:7115/api/Home";
+            var response = new HttpClient().GetStringAsync(urlRequest).Result;
+            List<Compounding> compounding = JsonConvert.DeserializeObject<List<Compounding>>(response);
+            return View(compounding);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 
-    public IActionResult Index()
+    public class Compounding
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public int sothang { get; set; }
+        public long sotien { get; set; }
+        public double tylelai { get; set; }
+        public double lai { get; set; }
     }
 }
-
